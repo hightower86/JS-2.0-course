82ad7efdd5e6708af150566b0f24445b0e37416a
Adel <htwr86@gmail.com>
2019-09-08 21:41:09 +0300

    task 7 OK

--- lesson_03/js/script.js
+++ lesson_03/js/script.js
@@ -41,15 +41,84 @@ const body = document.querySelector('body');
 const inp41 = document.querySelector('#i41');
 const inp42 = document.querySelector('#i42');
 const btn43 = document.querySelector('#b43');
+const result4 = document.querySelector('#res4');
 
 btn43.onclick = function() {
   const a = +inp41.value;
   const b = +inp42.value;
   if (a > b) {
-    body.innerHTML += a;
+    result4.value += a;
   } else if (b > a) {
-    body.innerHTML += b;
+    result4.value += b;
   } else {
-    body.innerHTML += 'равны!';
+    result4.value += 'равны!';
   }
+};
+
+// Task 5. Создайте на странице input, куда пользователь может
+// вводить год рождения. Добавьте кнопку. При нажатии
+// кнопки выполняйте функцию, которая если год рождения
+// меньше 2000 выводит на страницу возраст пользователя
+// (2019 - введенный год рождения),
+// если год рождения больше или равен 2000 - выводит в
+// консоль возраст пользователя.
+const inp5 = document.querySelector('#i5');
+const btn5 = document.querySelector('#b5');
+const result5 = document.querySelector('#res5');
+
+btn5.onclick = function() {
+  const a = +inp5.value;
+  if (a < 2000) {
+    result5.value += 2019 - a;
+  } else {
+    console.log(2019 - a);
+  }
+};
+inp5.onchange = function() {
+  result5.value = '';
+};
+
+// Task 6.
+// Создайте input куда пользователь может ввести номер
+// квартиры. Если квартира от 1 до 32 - то выводите
+// сообщение, о том, что такая квартира есть,если
+// другое число - сообщение о том, что квартира не
+// существует.
+const inp6 = document.querySelector('#i6');
+const btn6 = document.querySelector('#b6');
+const result6 = document.querySelector('#res6');
+
+btn6.onclick = function() {
+  const a = +inp6.value;
+  if (a <= 32) {
+    result6.value = 'квартира есть';
+  } else {
+    result6.value = 'такой квартиры нет';
+  }
+};
+inp6.onchange = function() {
+  result6.value = '';
+};
+
+// Task 7.
+// Создайте input куда пользователь может ввести число.
+// После нажатия кнопки - на страницу должно выводиться
+// сообщение - большие или меньше нуля это число.
+
+const inp7 = document.querySelector('#i7');
+const btn7 = document.querySelector('#b7');
+const result7 = document.querySelector('#res7');
+
+btn7.onclick = function() {
+  const a = +inp7.value;
+  if (a > 0) {
+    result7.value = 'число больше нуля';
+  } else if (a === 0) {
+    result7.value = 'число равно нулю';
+  } else {
+    result7.value = 'число меньше нуля';
+  }
+};
+inp7.onchange = function() {
+  result7.value = '';
 };
