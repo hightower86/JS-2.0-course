816110b95118af806c5a6364fc2ca299118fdecd
Adel <htwr86@gmail.com>
2019-09-10 19:45:59 +0300

    unit 4 task 5-8 OK

--- unit_04/script.js
+++ unit_04/script.js
@@ -28,3 +28,49 @@ const btn4 = document.querySelector('.btn4');
 btn4.onclick = () => {
   alert(check4.checked);
 };
+
+// task 5 --------------------
+
+const check5 = document.querySelector('.check5');
+const btn5 = document.querySelector('.btn5');
+btn5.onclick = () => {
+  if (check5.checked) {
+    alert(check5.value);
+  } else {
+    alert(check5.checked);
+  }
+};
+
+// task 6 --------------------
+
+const inp6 = document.querySelector('.inp6');
+const btn6 = document.querySelector('.btn6');
+btn6.onclick = () => {
+  alert(inp6.value);
+};
+
+// task 7 --------------------
+
+const inp7 = document.querySelector('.inp7');
+const btn7 = document.querySelector('.btn7');
+btn7.onclick = () => {
+  if (inp7.value.length < 6) {
+    console.log('длина пароля должна быть не меньше 6 символов');
+  } else {
+    alert(inp7.value);
+  }
+};
+
+// task 8 --------------------
+
+const btn8 = document.querySelector('.btn8');
+btn8.onclick = () => {
+  const inp8 = document.querySelector('.div8');
+  inp8.innerHTML += '<input class="newInp8" type="text"/>';
+  inp8.innerHTML += '<input class="newBtn8" type="button" value="button 8"/>';
+  const newBtn8 = document.querySelector('.newBtn8');
+  const newInp8 = document.querySelector('.newInp8');
+  newBtn8.onclick = () => {
+    alert(newInp8.value);
+  };
+};
