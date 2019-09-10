4df9837e801958dce8f8ea8cdec838ce08e2a226
Adel <htwr86@gmail.com>
2019-09-10 18:49:13 +0300

    unit 4 task 3-4 OK

--- unit_04/script.js
+++ unit_04/script.js
@@ -12,3 +12,19 @@ taskNumber = 2;
 btn2.onclick = () => {
   alert(taskNumber);
 };
+
+// task 3 --------------------
+
+const p3 = document.querySelector('.p3');
+taskNumber = 3;
+p3.onclick = () => {
+  alert(taskNumber);
+};
+
+// task 4 --------------------
+
+const check4 = document.querySelector('.check4');
+const btn4 = document.querySelector('.btn4');
+btn4.onclick = () => {
+  alert(check4.checked);
+};
