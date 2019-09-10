a2e41892fce25f37730d030c70dab885b565756d
Adel <htwr86@gmail.com>
2019-09-10 18:32:35 +0300

    unit 4 task 1-2 OK

---
+++ unit_04/script.js
@@ -0,0 +1,14 @@
+// task 1 --------------------
+const btn = document.querySelector('button');
+let taskNumber = 1;
+btn.onclick = () => {
+  alert(taskNumber);
+};
+
+// task 2 --------------------
+
+const btn2 = document.querySelector('.btn-2');
+taskNumber = 2;
+btn2.onclick = () => {
+  alert(taskNumber);
+};
