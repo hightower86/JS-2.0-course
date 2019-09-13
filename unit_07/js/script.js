ed4cd1a5adf8f559152ceb7a5a13fafe68e95c99
Adel <htwr86@gmail.com>
2019-09-13 15:15:51 +0300

    unit 7 task 4-10 complete

--- unit_07/js/script.js
+++ unit_07/js/script.js
@@ -25,3 +25,43 @@ f3 = num => {
 btn3.onclick = () => f3(12);
 
 // task 4 ------------------------
+btn4 = document.querySelector('.btn4');
+f4 = () => {
+  btn4.style.background = 'red';
+};
+
+btn4.onclick = () => f4();
+
+// task 5 ----------------------
+const inp5 = document.querySelector('.inp5');
+
+const f5 = () => {
+  return inp5.value;
+};
+
+//inp5.oninput = () => console.log(f5());
+
+// task 6 ------------------------
+const f6 = (num1, num2) => {
+  return num1 >= num2 ? num1 : num2;
+};
+
+// task 7 ------------------------
+const f7 = year => {
+  return 2019 - year;
+};
+
+// task 8 ------------------------
+const f8 = () => {
+  return Math.floor(Math.random() * 10);
+};
+
+// task 9 ------------------------
+const f9 = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+// task 10 ------------------------
+const f10 = () => {
+  return `rgb(${f9(0, 255)},${f9(0, 255)},${f9(0, 255)})`;
+};
