273dc4045c31fc8a421d4249f41a052f4909eef8
Adel <htwr86@gmail.com>
2019-09-15 10:01:34 +0300

    unit 09 initial commit

---
+++ unit_09/script.js
@@ -0,0 +1,9 @@
+// task 1 --------------------
+const pu1 = document.querySelector('.u-1');
+const btn1 = document.querySelector('.btn1');
+
+const func_1 = () => {
+  pu1.style.width = '200px';
+};
+
+btn1.onclick = () => func_1();
