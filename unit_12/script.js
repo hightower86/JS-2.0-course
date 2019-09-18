93dfad06986807545b089c2c4f2752d1603fef90
Adel <htwr86@gmail.com>
2019-09-18 13:52:16 +0300

    unit 12 task 10

--- unit_12/script.js
+++ unit_12/script.js
@@ -45,14 +45,73 @@ btn5.onclick = () => {
 };
 
 // task 6 -----------------------------
+const div6 = document.querySelector('.div6');
+const btn6 = document.querySelector('.btn6');
+
+btn6.onclick = () => {
+  div6.innerHTML = '';
+  for (let i = 0; i < a.length; i++) {
+    if (i % 2 === 0) {
+      div6.innerHTML += a[i] + '<br>';
+    }
+  }
+};
 
 // task 7 -----------------------------
+const div7 = document.querySelector('.div7');
+const btn7 = document.querySelector('.btn7');
+
+btn7.onclick = () => {
+  div7.innerHTML = '';
+  for (let i = 0; i < a.length; i++) {
+    for (let k = 0; k < a[i].length; k++) {
+      const el = a[i][k];
+      if (typeof el === 'number') {
+        div7.innerHTML += el;
+      }
+    }
+  }
+};
 
 // task 8 --------------------------------
+const div8 = document.querySelector('.div8');
+const btn8 = document.querySelector('.btn8');
+
+btn8.onclick = () => {
+  div8.innerHTML = '';
+  for (let i = 0; i < a.length; i++) {
+    div8.innerHTML += a[i].length + ' ';
+  }
+};
 
 // task 9 --------------------------
+const div9 = document.querySelector('.div9');
+const btn9 = document.querySelector('.btn9');
+let out9 = '';
+
+btn9.onclick = () => {
+  for (let i = a.length - 1; i >= 0; i--) {
+    for (let k = a[i].length - 1; k >= 0; k--) {
+      out9 += a[i][k] + ' ';
+    }
+  }
+  div9.innerHTML = out9;
+};
 
 // task 10 ----------------------------
+const div10 = document.querySelector('.div10');
+const btn10 = document.querySelector('.btn10');
+let out10 = '';
+
+btn10.onclick = () => {
+  for (let i = 0; i < a.length; i++) {
+    for (let k = a[i].length - 1; k >= 0; k--) {
+      out10 += a[i][k] + ' ';
+    }
+    out10 += '<br>';
+  }
+  div10.innerHTML = out10;
+};
 
 // task 11 ------------------------------
 
