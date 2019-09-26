7a1eb3b7b02f55645103c03c2912f325da70e0db
Adel <htwr86@gmail.com>
2019-09-26 16:54:34 +0300

    unit 16 task 07  OK

--- unit_16/script.js
+++ unit_16/script.js
@@ -39,13 +39,53 @@ btn3.onclick = () => {
 };
 
 // task 4 ------------------------------
+const btn4 = document.querySelector('.btn4');
+
+const func4 = () => {
+  const divs4 = document.querySelectorAll('.div3');
+
+  for (let div of divs4) {
+    div.innerHTML += 'task 5';
+  }
+};
+
+btn4.onclick = func4;
 
 // task 5 ------------------------------
+const btn5 = document.querySelector('.btn5');
+const divs5 = document.getElementsByClassName('div3'); //HTMLCollection
+// console.log(divs5);
 
-// task 6 -----------------------------
+const div5 = document.createElement('div');
+btn5.onclick = () => {
+  divs5.push(div5);
+}; // TypeError: divs5.push is not a function
+
+// task 6 --------------------------------
+const btn6 = document.querySelector('.btn6');
+const divs6 = document.querySelectorAll('.div3'); //NodeList
+// console.log(divs6);
+
+const div6 = document.createElement('div');
+btn6.onclick = () => {
+  divs6.push(div6);
+}; // TypeError: divs6.push is not a function
 
 // task 7 -----------------------------
+const btn7 = document.querySelector('.btn7');
+const a7 = [[1, 2], [3, 4], [5, 6]];
+const a7new = [];
 
+const func7 = () => {
+  for (let i = 0; i < a7.length; i++) {
+    for (let k = 0; k < a7[i].length; k++) {
+      a7new.push(a7[i][k]);
+    }
+  }
+  console.log(a7new);
+};
+
+btn7.onclick = func7;
 // task 8 --------------------------------
 
 // task 9 --------------------------
