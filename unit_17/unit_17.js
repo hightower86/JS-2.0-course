08b08e41baebfc83706a6b8edadf444c80b188e6
Adel <htwr86@gmail.com>
2019-09-27 17:57:34 +0300

    unit 18 task 5 OK

--- unit_17/unit_17.js
+++ unit_17/unit_17.js
@@ -84,34 +84,72 @@ document.querySelectorAll('.b-5').forEach(elem => {
 // Task 6 ============================================
 /*  Дан массив b6 = [3, 14, 15, 92, "6", "5", "hello", 32]. C помощью filter переберите массив b6 и создайте новый b6_res, который содержит только числа из b6. Действия должны запускаться при вызове функции t6. */
 
-function t6() {}
+function t6() {
+  const b6 = [3, 14, 15, 92, '6', '5', 'hello', 32];
+  b6_res = b6.filter(function(item) {
+    return typeof item === 'number';
+  });
+  document.querySelector('.out-6').innerHTML = b6_res;
+}
 
 document.querySelector('.b-6').onclick = t6;
 
 // Task 7 ============================================
 /*  Дан массив b7 = ["php-7", "html", "css", 92, "6", "5", "hello", 32]. C помощью filter переберите массив b7 и создайте новый b7_res, который содержит только строки из b7, длина которых больше 3. Действия должны запускаться при вызове функции t7. */
 
-function t7() {}
+function t7() {
+  const b7 = ['php-7', 'html', 'css', 92, '6', '5', 'hello', 32];
+  b7_res = b7.filter(function(item) {
+    return typeof item === 'string' && item.length > 3;
+  });
+  document.querySelector('.out-7').innerHTML = b7_res;
+}
 
 document.querySelector('.b-7').onclick = t7;
 
 // Task 8 ============================================
 /* Дан массив b8 = [3, 14, 15, 92, "6", "5", "hello", 32]. С помощью filter, переберите массив b8 и создайте новый массив b8_res, который содержит индексы четных элементов. Действия должны запускаться при вызове функции t8 */
 
-function t8() {}
+function t8() {
+  const b8 = [3, 14, 15, 92, '6', '5', 'hello', 32];
+  b8.filter(function(item, idx) {
+    if (item % 2 === 0) {
+      b8_res.push(idx);
+      return true;
+    }
+  });
+  document.querySelector('.out-8').innerHTML = b8_res;
+}
 
 document.querySelector('.b-8').onclick = t8;
 
 // Task 9 ============================================
 /*  Дан массив b9 = [3, "hello", 4, "world", 5, "hi"]. С помощью filter, переберите массив b5 и создайте новый массив b9_num и b9_string, которые содержат первый - числа из b9, второй - строки. Задачу решить одним filter. Действия должны запускаться при вызове функции t9. */
 
-function t9() {}
+function t9() {
+  const b9 = [3, 'hello', 4, 'world', 5, 'hi'];
+  b9.filter(function(item) {
+    if (typeof item === 'number') {
+      b9_num.push(item);
+    } else {
+      b9_string.push(item);
+    }
+  });
+  document.querySelector('.out-9').innerHTML = b9_num + '<br>' + b9_string;
+}
 
 document.querySelector('.b-9').onclick = t9;
 
 // Task 10 ============================================
 /*  Дан массив b10 = [ [1,2,3], [3,4,6], [4,5,7], [8,9,3]]. С помощью filter переберите массив и создайте новый b10_res, в который входят вложенные массивы содержащие цифру 3. Действия должны запускаться при вызове функции t10. */
 
-function t10() {}
+function t10() {
+  const b10 = [[1, 2, 3], [3, 4, 6], [4, 5, 7], [8, 9, 3]];
+  b10_res = b10.filter(function(arr) {
+    return arr.includes(3);
+  });
+  console.log(b10_res);
+  document.querySelector('.out-10').textContent = b10_res;
+}
 
 document.querySelector('.b-10').onclick = t10;
