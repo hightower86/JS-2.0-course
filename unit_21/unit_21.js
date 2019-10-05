e05f3323ad4fc53b36c63326225daf3efd8de9b9
Adel <htwr86@gmail.com>
2019-10-05 16:35:39 +0300

    unit-21 task 5 OK

--- unit_21/unit_21.js
+++ unit_21/unit_21.js
@@ -31,18 +31,24 @@ document.querySelector('.div-3_1').addEventListener('touchstart', t3);
 document.querySelector('.div-3_2').addEventListener('touchstart', t3);
 // Task 4 ============================================
 /*  Создайте блок div-4. И кнопку b-4. При нажатии кнопки - добавляйте событие ontouchstart на блок div-4. При событии происходит вывод текста touch в out-4.  */
-
-function t4() {}
+const div4 = document.querySelector('.div-4');
+function t4() {
+  document.querySelector('.out-4').textContent += 'touch';
+}
 
 // ваше событие здесь!!!
+document.querySelector('.b-4').onclick = () =>
+  div4.addEventListener('touchstart', t4);
 
 // Task 5 ============================================
 /*  Дана кнопка b-5. При ее нажатии очищайте событие ontouchstart на блоке div-4. */
 
-function t5() {}
+function t5() {
+  div4.removeEventListener('touchstart', t4);
+}
 
 // ваше событие здесь!!!
-
+document.querySelector('.b-5').onclick = t5;
 // Task 6 ============================================
 /*  Добавьте событие ontouchend на div-4. При его срабатывании выведите в out-6 слово touchend. */
 
